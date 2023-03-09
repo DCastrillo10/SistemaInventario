106ca27825f87a4f39a03a01d617bbd6e45f02f2
Deimer Castrillo <dcastrillomartinez@gmail.com>
2023-03-09 18:38:03 -0500

    ConfiguracionProyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        [MaxLength(60, ErrorMessage ="{0} debe ser maximo de {1} caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(100, ErrorMessage = "{0} debe ser maximo de {1} caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public bool Estado { get; set; }
+    }
+}
