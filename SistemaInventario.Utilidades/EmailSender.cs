aeec5eddb80ac0f2d86aaa5f80b5e0362b1a8f14
Deimer Castrillo <dcastrillomartinez@gmail.com>
2023-04-23 10:49:07 -0500

    Email2

--- SistemaInventario.Utilidades/EmailSender.cs
+++ SistemaInventario.Utilidades/EmailSender.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,21 @@ namespace SistemaInventario.Utilidades
 {
     public class EmailSender : IEmailSender
     {
+        public string SendGridSecret { get; set; }
+
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+        }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();    
+            var cliente = new SendGridClient(SendGridSecret);
+            var from = new EmailAddress("dcastrillomartinez@gmail.com");
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+            return cliente.SendEmailAsync(msg);
         }
     }
 }
